bf853daab65467bb9cdc204c7b3d38a258d86481
Sarah Darnell <sarahd1707@gmail.com>
2025-07-25 15:36:14 -0500

    Changed way renaming occurs, created table 2

--- R files/VBT Predictive Factors Part 1.R
+++ R files/VBT Predictive Factors Part 1.R
@@ -1,5 +1,5 @@
 #VBT Predictive Factors ~ part 1
-#Written by Sarah Darnell, last modified 7.22.25
+#Written by Sarah Darnell, last modified 7.25.25
 
 library(readr)
 library(dplyr)
@@ -46,8 +46,7 @@ redcap <- redcap %>%
   mutate(mh_assigned_sex = case_match(
     mh_assigned_sex, 
     1 ~ "Female"
-  )) %>%
-  rename(`Assigned Sex at Birth` = mh_assigned_sex)
+  )) 
 #gender
 redcap <- redcap %>%
   mutate(multi_gender = ifelse(rowSums(
@@ -113,8 +112,7 @@ redcap <- redcap %>%
     1 ~ "Hispanic or Latino", 
     2 ~ "Not Hispanic or Latino", 
     3 ~ "Uknown"
-  )) %>%
-  rename(Ethnicity = mh4_ethnicity)
+  )) 
 #education
 redcap <- redcap %>%
   mutate(mh5_education = case_match(
@@ -125,8 +123,7 @@ redcap <- redcap %>%
     4 ~ "Associate's Degree", 
     5 ~ "Bachelor's Degree", 
     6 ~ "Post-Graduate Degree"
-  )) %>%
-  rename(Education = mh5_education)
+  )) 
 #employment
 redcap <- redcap %>%
   mutate(multi_employment = ifelse(rowSums(
@@ -161,37 +158,32 @@ redcap <- redcap %>%
     mh_smoking, 
     1 ~ "Yes", 
     0 ~ "No"
-  )) %>%
-  rename(`Nicotine Usage` = mh_smoking)
+  )) 
 #THC
 redcap <- redcap %>%
   mutate(ms_thc = case_match(
     ms_thc, 
     1 ~ "Yes", 
     0 ~ "No"
-  )) %>%
-  rename(`THC (Marijuana) Usage` = ms_thc)
+  )) 
 #Problem with drugs or alcohol
 redcap <- redcap %>%
   mutate(mh9b1 = case_match(
     mh9b1, 
     1 ~ "Yes", 
     0 ~ "No"
-  )) %>%
-  rename(`Have you ever had a problem with drugs or alcohol?` = mh9b1)
+  )) 
 #births, also forces text input into numerics (visually inspected, a few pts
 #wrote in 0, so coercing text to 0)
 redcap <- redcap %>%
-  rename(`Vaginal Births` = mh15_vagbirths, 
-         `Caesarean Section Births` = mh14_csection) %>%
   mutate(across(
-    c(`Vaginal Births`, `Caesarean Section Births`),
-    ~ ifelse(
+    c(mh15_vagbirths, mh14_csection),
+    ~ as.numeric(ifelse(
       redcap_event_name == "virtual_assessment_arm_1",
       ifelse(is.na(as.numeric(.x)), 0, as.numeric(.x)),
       .x
-    )
-  ))
+    ))
+  )) 
 #income
 redcap <- redcap %>%
   mutate(mh_income = case_match(
@@ -203,13 +195,26 @@ redcap <- redcap %>%
     5 ~ "$100,000 - < $150,000", 
     6 ~ "$150,000 or greater", 
     7 ~ "Unknown"
-  )) %>%
-  rename(Income = mh_income)  
+  )) 
   
 #saving file
 write_csv(redcap, "Edited data files/redcap.csv")  
 
 #Defining vars for table 1
+redcap_table1 <- redcap %>%
+  filter(redcap_event_name == "virtual_assessment_arm_1") %>%
+  rename(
+    `Assigned Sex at Birth` = mh_assigned_sex, 
+    Ethnicity = mh4_ethnicity, 
+    Education = mh5_education, 
+    `Nicotine Usage` = mh_smoking, 
+    `THC (Marijuana) Usage` = ms_thc, 
+    `Have you ever had a problem with drugs or alcohol?` = mh9b1, 
+    `Vaginal Births` = mh15_vagbirths, 
+    `Caesarean Section Births` = mh14_csection, 
+    Income = mh_income
+  )
+
 vars <- c("Age", "Assigned Sex at Birth", "Gender", "Race", "Ethnicity", 
           "Education", "Employment", "Income", "Vaginal Births", 
           "Caesarean Section Births", "Nicotine Usage", "THC (Marijuana) Usage", 
@@ -218,8 +223,7 @@ factor_vars <- c("Assigned Sex at Birth", "Gender", "Race", "Ethnicity",
                  "Education", "Employment", "Income", "Nicotine Usage", 
                  "THC (Marijuana) Usage", 
                  "Have you ever had a problem with drugs or alcohol?")
-redcap_table1 <- redcap %>%
-  filter(redcap_event_name == "virtual_assessment_arm_1")
+
 
 #Creating table
 demo <- CreateTableOne(vars, data = redcap_table1, factorVars = factor_vars, 
@@ -296,155 +300,114 @@ redcap <- redcap %>%
     mh_ocps, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`Oral Contraceptive Pills` = mh_ocps)  
+  )) 
 #patch
 redcap <- redcap %>%
   mutate(mh_patch = case_match(
     mh_patch, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`Contraceptive Patch` = mh_patch) 
+  )) 
 #ring
 redcap <- redcap %>%
   mutate(mh_ring = case_match(
     mh_ring, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`Vaginal Ring` = mh_ring)
+  )) 
 #implant
 redcap <- redcap %>%
   mutate(mh_implant = case_match(
     mh_implant, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`Progestin Implant` = mh_implant)
+  )) 
 #shot
 redcap <- redcap %>%
   mutate(mh_shot = case_match(
     mh_shot, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`Progestin Shot` = mh_shot)
+  )) 
 #pills
 redcap <- redcap %>%
   mutate(mh_p_pills = case_match(
     mh_p_pills, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`Progestin Pills` = mh_p_pills)
+  )) 
 #gnrh agonist
 redcap <- redcap %>%
   mutate(mh_gnrh_agonist = case_match(
     mh_gnrh_agonist, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`GnRH Agonists` = mh_gnrh_agonist)
+  ))
 #gnrh antagonist
 redcap <- redcap %>%
   mutate(mh_gnrh_antagonist = case_match(
     mh_gnrh_antagonist, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`GnRH Antagonists` = mh_gnrh_antagonist)
+  )) 
 #hormonal iud
 redcap <- redcap %>%
   mutate(mh_iudh = case_match(
     mh_iudh, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`Hormonal IUD` = mh_iudh)
+  )) 
 #copper iud
 redcap <- redcap %>%
   mutate(mh_iudc = case_match(
     mh_iudc, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`Copper IUD` = mh_iudc)
+  ))
 #Aromatase
 redcap <- redcap %>%
   mutate(mh_aromatase = case_match(
     mh_aromatase, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`Aromatase Inhibitors` = mh_aromatase)
+  )) 
 #other
 redcap <- redcap %>%
   mutate(mh_hormonal_other = case_match(
     mh_hormonal_other, 
     1 ~ "Currently taking", 
     2 ~ "Have taken in the past"
-  )) %>%
-  rename(`Other Hormonal Medication` = mh_hormonal_other)
+  ))
 #period in last 6 months
 redcap <- redcap %>%
   mutate(mh_period_6months = case_match(
     mh_period_6months, 
     1 ~ "Yes", 
     0 ~ "No"
-  )) %>%
-  rename(`Period in the last 6 months?` = mh_period_6months)
+  )) 
 #painful periods
 redcap <- redcap %>%
   mutate(mh18_painfulperiodsyn = case_match(
     mh18_painfulperiodsyn, 
     1 ~ "Yes", 
     0 ~ "No"
-  )) %>%
-  rename(`Ever had painful periods?` = mh18_painfulperiodsyn)
+  )) 
 #painful periods at time of menarche
 redcap <- redcap %>%
   mutate(mh19 = case_match(
     mh19, 
     1 ~ "Yes", 
     0 ~ "No"
-  )) %>%
-  rename(`Painful periods at time of menarche?` = mh19)
-#painful periods age, if not menarche
+  )) 
+#total years without a period (making new var with only 0-10 numeric, keeping
+#old var for reference later)
 redcap <- redcap %>%
-  rename(`What age did your painful periods start, if not menarche?` = mh19a)
-#total years without a period
-redcap <- redcap %>%
-  mutate(mh19b = as.character(mh19b)) %>%
   mutate(mh19b = case_when(
-    mh19b == 999 ~ "Never, always had regular periods", 
-    mh19b == 0 ~ "Less than 1 year", 
-    mh19b == 11 ~ "More than 10 years", 
+    mh19b == 999 ~ NA, 
+    mh19b == 0 ~ NA, 
+    mh19b == 11 ~ NA, 
     TRUE ~ mh19b
-  )) %>%
-  rename(`Total years since menarche without a period` = mh19b)
-#days of menstrual pain >= 4 a month
-redcap <- redcap %>%
-  rename(`Days with menstrual pelvic pain >= 4 in an average month` = mh20)
-#days of work/school/activities missed due to painful periods last 3 months
-redcap <- redcap %>%
-  rename(`Days of missed work, school, or activities due 
-          to painful period in last 3 months` = mh21)
-#days in bed due to painful periods last 3 months
-redcap <- redcap %>%
-  rename(`Days spent in bed due to painful period in last 3 months` = mh22)
-#period pain last 3 months with no pain relievers
-redcap <- redcap %>%
-  rename(`Average pain during worst day of period when not taking pain relievers
-          in last 3 months (VAS)` = mh23)
-#period pain last 3 months with nsaid pain relievers
-redcap <- redcap %>%
-  rename(`Average pain during worst day of period when taking NSAID 
-          pain relievers in last 3 months (VAS)` = mh23a)
-#period pain last 3 months with acetaminophen pain relievers
-redcap <- redcap %>%
-  rename(`Average pain during worst day of period when taking acetaminophen 
-          pain relievers in last 3 months (VAS)` = mh27b)
+  ))
 #menstrual cycle regularity 
 redcap <- redcap %>%
   mutate(mh24 = case_match(
@@ -453,8 +416,7 @@ redcap <- redcap %>%
     1 ~ "Sometimes irregular", 
     2 ~ "Always regular", 
     4 ~ "Using a type of birth control that prevents periods"
-  )) %>%
-  rename(`Menstrual Cycle Regularity` = mh24)
+  ))
 #menstrual cycle length
 redcap <- redcap %>%
   mutate(mh25 = case_match(
@@ -462,8 +424,7 @@ redcap <- redcap %>%
     1 ~ "1-21 days", 
     2 ~ "22-35 days", 
     3 ~ "Greater than 35 days"
-  )) %>%
-  rename(`Menstrual Cycle Length` = mh25)
+  ))
 #menstrual phase
 redcap <- redcap %>%
   mutate(
@@ -478,5 +439,173 @@ redcap <- redcap %>%
     `day of cycle` <= 14 ~ "Follicular Phase", 
     `day of cycle` >= 15 ~ "Luteal Phase"
   ))
+#avg days of bleeding per period (making new var with only 0-10 numeric, 
+#keeping old var for reference later)
+redcap <- redcap %>%
+  mutate(mh27 = case_when(
+    mh27 == 11 ~ NA, 
+    TRUE ~ mh27
+  ))
+#Hormonal therapy to treat menstrual pain
+redcap <- redcap %>%
+  mutate(mh28 = case_match(
+    mh28, 
+    1 ~ "Yes", 
+    0 ~ "No"
+  )) 
+#bladder, bowel, abdominal pain outside of period
+redcap <- redcap %>%
+  mutate(mh30 = case_match(
+    mh30, 
+    1 ~ "Yes", 
+    0 ~ "No"
+  ))
+#tampon test pain (making new var with only 0-10 numeric, 
+#keeping old var for reference later)
+redcap <- redcap %>%
+  mutate(tampon_test = case_when(
+    tampon_test == 99 ~ NA, 
+    tampon_test == 0 ~ 0,
+    tampon_test == 10 ~ 10, 
+    TRUE ~ tampon_test
+  ))
+
+#saving file
+write_csv(redcap, "Edited data files/redcap.csv")  
+
+#Defining vars for table 1
+redcap_table2 <- redcap %>%
+  filter(redcap_event_name == "virtual_assessment_arm_1") %>%
+  rename(
+    `Oral Contraceptive Pills` = mh_ocps,
+    `Contraceptive Patch` = mh_patch,
+    `Vaginal Ring` = mh_ring,
+    `Progestin Implant` = mh_implant,
+    `Progestin Shot` = mh_shot,
+    `Progestin Pills` = mh_p_pills,
+    `GnRH Agonists` = mh_gnrh_agonist,
+    `GnRH Antagonists` = mh_gnrh_antagonist,
+    `Hormonal IUD` = mh_iudh,
+    `Copper IUD` = mh_iudc,
+    `Aromatase Inhibitors` = mh_aromatase,
+    `Other Hormonal Medication` = mh_hormonal_other,
+    `Period in the last 6 months?` = mh_period_6months,
+    `Ever had painful periods?` = mh18_painfulperiodsyn,
+    `Painful periods at time of menarche?` = mh19,
+    `What age did your painful periods start, if not menarche?` = mh19a,
+    `Years since menarche without a period` = mh19b,
+    `Days with menstrual pelvic pain >= 4 in an average month` = mh20,
+    `Days of missed work, school, or activities due to painful period in last 3 months` = mh21,
+    `Days spent in bed due to painful period in last 3 months` = mh22,
+    `Average pain during worst day of period when not taking pain relievers in last 3 months (VAS)` = mh23,
+    `Average pain during worst day of period when taking NSAID pain relievers in last 3 months (VAS)` = mh23a,
+    `Average pain during worst day of period when taking acetaminophen pain relievers in last 3 months (VAS)` = mh27b,
+    `Menstrual Cycle Regularity` = mh24,
+    `Menstrual Cycle Length` = mh25,
+    `Average bleeding days per period` = mh27,
+    `Used hormonal therpary to treat menstrual pain` = mh28,
+    `Bladder, bowel, or abdomino-pelvic pain outside of period in last 3 months` = mh30,
+    `Pain with tampon test` = tampon_test
+  )
+
+vars <- c("Oral Contraceptive Pills", "Contraceptive Patch", "Vaginal Ring", 
+          "Progestin Implant", "Progestin Shot", "Progestin Pills", 
+          "GnRH Agonists", "GnRH Antagonists", "Hormonal IUD", "Copper IUD", 
+          "Aromatase Inhibitors", "Other Hormonal Medication", 
+          "Period in the last 6 months?", "Ever had painful periods?", 
+          "Painful periods at time of menarche?", 
+          "What age did your painful periods start, if not menarche?", 
+          "Years since menarche without a period", 
+          "Days with menstrual pelvic pain >= 4 in an average month", 
+          "Days of missed work, school, or activities due to painful period in last 3 months", 
+          "Days spent in bed due to painful period in last 3 months", 
+          "Average pain during worst day of period when not taking pain relievers in last 3 months (VAS)", 
+          "Average pain during worst day of period when taking NSAID pain relievers in last 3 months (VAS)", 
+          "Average pain during worst day of period when taking acetaminophen pain relievers in last 3 months (VAS)", 
+          "Menstrual Cycle Regularity", "Menstrual Cycle Length", 
+          "Menstrual Cycle Phase", "Average bleeding days per period", 
+          "Used hormonal therpary to treat menstrual pain", 
+          "Bladder, bowel, or abdomino-pelvic pain outside of period in last 3 months", 
+          "Pain with tampon test")
+
+factor_vars <- c("Oral Contraceptive Pills", "Contraceptive Patch", "Vaginal Ring", 
+                 "Progestin Implant", "Progestin Shot", "Progestin Pills", 
+                 "GnRH Agonists", "GnRH Antagonists", "Hormonal IUD", "Copper IUD", 
+                 "Aromatase Inhibitors", "Other Hormonal Medication", 
+                 "Period in the last 6 months?", "Ever had painful periods?", 
+                 "Painful periods at time of menarche?", "Menstrual Cycle Regularity", 
+                 "Menstrual Cycle Length", "Menstrual Cycle Phase",
+                  "Used hormonal therpary to treat menstrual pain", 
+                  "Bladder, bowel, or abdomino-pelvic pain outside of period in last 3 months")
+
+#Creating table
+demo <- CreateTableOne(vars, data = redcap_table2, factorVars = factor_vars, 
+                       strata = "Group")
+
+demo_df <- as.data.frame(print(demo, 
+                               nonnormal = c("What age did your painful periods start, if not menarche?",
+                                             "Years since menarche without a period",
+                                             "Days with menstrual pelvic pain >= 4 in an average month", 
+                                             "Days of missed work, school, or activities due to painful period in last 3 months", 
+                                             "Days spent in bed due to painful period in last 3 months",
+                                             "Average pain during worst day of period when not taking pain relievers in last 3 months (VAS)",
+                                             "Average pain during worst day of period when taking NSAID pain relievers in last 3 months (VAS)",
+                                             "Average pain during worst day of period when taking acetaminophen pain relievers in last 3 months (VAS)", 
+                                             "Pain with tampon test"),
+                               printToggle = FALSE,
+                               quote = FALSE,
+                               noSpaces = TRUE,
+                               showAllLevels = TRUE))
+
+# Remove p-value/test columns
+cols_to_remove <- c("p", "test")
+demo_df <- demo_df[, !colnames(demo_df) %in% cols_to_remove]
+
+#Step 1: save rownames as a column
+demo_df <- data.frame(rowname = rownames(demo_df), demo_df, row.names = NULL)
+
+# Step 2: Create an empty output data frame
+restructured_df <- data.frame()
+
+# Step 3: Loop through rows and insert variable name before its levels
+current_var <- NA
+
+for (i in seq_len(nrow(demo_df))) {
+  row_label <- demo_df$rowname[i]
+  if (!startsWith(row_label, "  ")) {
+    # Continuous variable row — use as is
+    current_var <- row_label
+    new_row <- demo_df[i, ]
+    new_row$Variable <- current_var
+    restructured_df <- bind_rows(restructured_df, new_row)
+  } else {
+    # Categorical level — insert variable name row if not already added
+    if (!identical(tail(restructured_df$Variable, 1), current_var)) {
+      var_row <- demo_df[i, ]
+      var_row[2:ncol(var_row)] <- ""  # clear values
+      var_row$Variable <- current_var
+      restructured_df <- bind_rows(restructured_df, var_row)
+    }
+    level_row <- demo_df[i, ]
+    level_row$Variable <- ""
+    restructured_df <- bind_rows(restructured_df, level_row)
+  }
+}
+
+# Step 4: Drop original rowname and reorder
+restructured_df <- restructured_df[, c("Variable", setdiff(names(restructured_df), c("rowname", "Variable")))]
 
 
+#building flextable
+ft <- flextable(demo_df) %>%
+  bold(i = which(demo_df$Variable != ""), j = 1) %>%      # Bold variable rows
+  align(align = "left", part = "all") %>%                 # Align left
+  fontsize(size = 9, part = "all") %>%                    # Reduce font size
+  set_table_properties(layout = "fixed", width = 1) %>%   # Fixed width layout
+  width(j = 1, width = 2.25) %>%                          # Widen first column for variable names
+  width(j = 2:ncol(demo_df), width = 1.25) %>%            # Narrow group columns
+  theme_vanilla()
+
+read_docx() %>%
+  body_add_flextable(ft) %>%
+  print(target = "Tables/VBT_Predictive_Factors_Table2.docx")
