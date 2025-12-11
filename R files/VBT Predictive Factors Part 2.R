fc75af2d0bce8ac7fb912074cdabcbdcc96bada4
Sarah Darnell <sarahd1707@gmail.com>
2025-12-11 15:48:20 -0600

    Regression models

--- R files/VBT Predictive Factors Part 2.R
+++ R files/VBT Predictive Factors Part 2.R
@@ -1,5 +1,5 @@
 #VBT Predictive Factors ~ part 2 (graphs)
-#Written by Sarah Darnell, last modified 12.8.25
+#Written by Sarah Darnell, last modified 12.11.25
 
 library(readr)
 library(dplyr)
@@ -63,10 +63,12 @@ ggplot(redcap, aes(x = Group, y = gupi_bl)) +
 correlation_subset <- redcap %>%
   filter(redcap_event_name == "virtual_assessment_arm_1") %>%
   select(record_id, vbt_fu_pain, bl_urine_ml, 
-         gupi_bl, icsi_bl, gsrs_bl)
+         gupi_bl, icsi_bl, gsrs_bl, mcgill_1,
+         mcgill_2, mcgill_3)
 
 vars <- correlation_subset %>% select(vbt_fu_pain, bl_urine_ml, 
-                                      gupi_bl, icsi_bl, gsrs_bl)
+                                      gupi_bl, icsi_bl, gsrs_bl,
+                                      mcgill_1, mcgill_2, mcgill_3)
 
 #spearman matrix
 spearman <- cor(vars, method = "spearman", use = "pairwise.complete.obs")
@@ -75,10 +77,47 @@ spearman <- cor(vars, method = "spearman", use = "pairwise.complete.obs")
 pearson <- cor(vars, method = "pearson", use = "pairwise.complete.obs")
 
 
+###########################################
+## Regression Models (GSRS, ICSI, GUPI ) ##
+###########################################
 
+#subset needed variables
+regression_subset <- redcap %>%
+  filter(redcap_event_name == "virtual_assessment_arm_1") %>%
+  select(record_id, vbt_fu_pain, bl_urine_ml, 
+         vbt_fs_pain, vbt_mt_pain, mcgill_1,
+         mcgill_2, mcgill_3)
 
+#Model #1: NMPP ~ FS pain + FU pain + MT pain + volume voided
+mcgill_1_model <- lm(mcgill_1 ~ 
+                       vbt_fs_pain + 
+                       vbt_fu_pain +
+                       vbt_mt_pain +
+                       bl_urine_ml, 
+                  data = regression_subset)
 
+#print summary of model
+summary(mcgill_1_model)
 
+#Model #2: Painful urination ~ FS pain + FU pain + MT pain + volume voided
+mcgill_2_model <- lm(mcgill_2 ~ 
+                       vbt_fs_pain + 
+                       vbt_fu_pain +
+                       vbt_mt_pain +
+                       bl_urine_ml, 
+                     data = regression_subset)
 
+#print summary of model
+summary(mcgill_2_model)
 
+#Model #3: Painful bowel movements ~ FS pain + FU pain + MT pain + volume voided
+mcgill_3_model <- lm(mcgill_3 ~ 
+                       vbt_fs_pain + 
+                       vbt_fu_pain +
+                       vbt_mt_pain +
+                       bl_urine_ml, 
+                     data = regression_subset)
+
+#print summary of model
+summary(mcgill_3_model)
 
